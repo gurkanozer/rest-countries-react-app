cbcbc3c359004d2999ca48323aa473ebfd050af7
gurkanozer <grknzr2609@gmail.com>
2022-04-02 23:39:47 +0300

    some animation issues

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import { CardContainer, CardHeader, CardBody, Flag, Title, List, Item, Strong, Paragraph } from './style';
+import React from "react";
+import {
+  CardContainer,
+  CardHeader,
+  CardBody,
+  Flag,
+  Title,
+  List,
+  Item,
+  Strong,
+  Paragraph,
+} from "./style";
 
-const Card = ({item,onClick}) => {
-  
+const Card = ({ item, onClick }) => {
   return (
-    <CardContainer 
-      onClick={()=>onClick(item)}
-      initial={{ scale:0.9 }}
-      animate={{ scale: 1 }}
-      transition={{ duration: 0.3 }}
-      whileHover={{ scale: 1.05 }}
-    >
+    <CardContainer layout onClick={() => onClick(item)} whileHover={{ y: -10 }}>
       <CardHeader>
         <Flag src={item.flags.png} />
       </CardHeader>
       <CardBody>
         <Title>{item.name}</Title>
         <List>
-          <Item><Paragraph><Strong>Population:</Strong>{item.population}</Paragraph></Item>
-          <Item><Paragraph><Strong>Region:</Strong>{item.region}</Paragraph></Item>
-          <Item><Paragraph><Strong>Capital:</Strong>{item.capital}</Paragraph></Item>
+          <Item>
+            <Paragraph>
+              <Strong>Population:</Strong>
+              {item.population}
+            </Paragraph>
+          </Item>
+          <Item>
+            <Paragraph>
+              <Strong>Region:</Strong>
+              {item.region}
+            </Paragraph>
+          </Item>
+          <Item>
+            <Paragraph>
+              <Strong>Capital:</Strong>
+              {item.capital}
+            </Paragraph>
+          </Item>
         </List>
       </CardBody>
     </CardContainer>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/layers/List/index.js
+++ src/layers/List/index.js
@@ -1,24 +1,22 @@
-import React  from 'react';
+import React from "react";
 
-import {ListContainer, ListItemContainer} from './style';
-import {Card} from '../../components';
+import { ListContainer, ListItemContainer } from "./style";
+import { Card } from "../../components";
 
-const List = ({countries, setSelectedCountry}) => {
+const List = ({ countries, setSelectedCountry }) => {
   return (
     <ListContainer
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
     >
-      {
-        countries?.map((country, index)=>(
-          <ListItemContainer key={index}>
-            <Card 
-              item={country} 
-              onClick={setSelectedCountry}
-              />
-          </ListItemContainer>
-        ))
-      }
+      {countries?.map((country, index) => (
+        <ListItemContainer layout key={index}>
+          <Card item={country} onClick={setSelectedCountry} />
+        </ListItemContainer>
+      ))}
     </ListContainer>
-  )
-}
+  );
+};
 
-export default List
+export default List;
